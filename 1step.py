b726a137292fc6e7b7dfb46e5b3457d8cd3e87e7
romant010 <48656710+romant010@users.noreply.github.com>
2019-05-07 11:05:17 +0300

    3 commit

--- 1step.py
+++ 1step.py
@@ -5,9 +5,7 @@ import selenium.webdriver.chrome.service as service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 chrome = webdriver.Chrome()
-try:
-    chrome.get('http://192.168.99.100:5000/')
-    print(chrome.title)
-# except:
-    # chrome.quit()
+chrome.get('http://192.168.99.100:5000/')
+print(chrome.title)
+
 
